92fb03991be7017ebde5523cb93d4f55cc4660b5
mihajlo <mihajloo966@gmail.com>
2021-01-06 21:03:18 +0100

    Web Services for Create, Read Categories and HTML Template

---
+++ OnlineShop/ApiServices/CategoryServices.cs
@@ -0,0 +1,29 @@
+﻿using OnlineShop.Database;
+using OnlineShop.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShop.ApiServices
+{
+    public class CategoryServices
+    {
+        public void CreateCategory(Category category)
+        {
+            using (var context = new DatabaseContext())
+            {
+                context.Categories.Add(category);
+                context.SaveChanges();
+            }
+        }
+
+        public List<Category> GetAllCategories()
+        {
+            using (var context = new DatabaseContext())
+            {
+                return context.Categories.ToList();
+            }
+        }
+    }
+}
---
+++ OnlineShop/Controllers/CategoryController.cs
@@ -0,0 +1,35 @@
+﻿using OnlineShop.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineShop.ApiServices;
+
+namespace OnlineShop.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly CategoryServices _apiService = new CategoryServices(); 
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            List<Category> categories = _apiService.GetAllCategories();
+            return View(categories);
+        }
+
+        [HttpGet] // Template Form for adding category
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            _apiService.CreateCategory(category);
+            return RedirectToAction("Index");
+        }
+    }
+}
