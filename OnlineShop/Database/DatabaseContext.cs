a9b3bcb6aa01ddc8a5b25c9f41c15d6dc96f74cf
mihajlo <mihajloo966@gmail.com>
2021-01-06 20:30:05 +0100

    Business Entities, EF Relation, DB Context

---
+++ OnlineShop/Database/DatabaseContext.cs
@@ -0,0 +1,17 @@
+﻿using OnlineShop.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace OnlineShop.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext() : base("WebShopDB") {}
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
