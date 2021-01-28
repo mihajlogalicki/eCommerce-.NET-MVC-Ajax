6974cebc3fb37eb7325c3611efd21d8f3726ed14
mihajlo <mihajloo966@gmail.com>
2021-01-28 06:23:08 +0100

    New migration added for product image entity, Repair Category Ajax added  with image

--- OnlineShop/Entities/Product.cs
+++ OnlineShop/Entities/Product.cs
@@ -12,6 +12,7 @@ namespace OnlineShop.Entities
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
 
         [ForeignKey("Category")]
         public int CategoryId { get; set; }
