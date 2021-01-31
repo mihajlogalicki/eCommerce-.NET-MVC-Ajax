9970c90a3e87b9e23cd3c1f93f1e97ed98867e10
mihajlo <mihajloo966@gmail.com>
2021-01-31 07:14:39 +0100

    Decoupling Complexity for controler PartialView and View method

--- OnlineShop/Controllers/ShopController.cs
+++ OnlineShop/Controllers/ShopController.cs
@@ -13,20 +13,16 @@ namespace OnlineShop.Controllers
         private readonly ProductServices _apiService = ProductServices.GetSingleInstance();
         private readonly CategoryServices _CatApiService = new CategoryServices();
 
-        public ActionResult index(string search, int? minPrice, int? maxPrice, int? categoryId, int? sortBy)
+        public ActionResult index()
         {
-            ShopViewModel ShopVM = new ShopViewModel();
-
-            ShopVM.Categories = _CatApiService.GetAllCategories();
-            ShopVM.maximumPrice = _apiService.GetMaximumPrice();
-            ShopVM.Products = _apiService.SearchProducts(search, minPrice, maxPrice, categoryId, sortBy);
-            return View(ShopVM);
+            return View();
         }
 
         public ActionResult ProductFilter(string search, int? minPrice, int? maxPrice, int? categoryId, int? sortBy)
         {
             ShopViewModel ShopVM = new ShopViewModel();
-
+            ShopVM.Categories = _CatApiService.GetAllCategories();
+            ShopVM.maximumPrice = _apiService.GetMaximumPrice();
             ShopVM.Products = _apiService.SearchProducts(search, minPrice, maxPrice, categoryId, sortBy);
             return PartialView(ShopVM);
         }
